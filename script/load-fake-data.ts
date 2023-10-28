330f4d0c3c77ab935a49222238115871c794b447
酒井専冴 <atsukisakai@sakaisensaenoMacBook-Pro.local>
2023-10-28 23:34:02 +0900

    args to load-fake-data.ts

--- script/load-fake-data.ts
+++ script/load-fake-data.ts
@@ -1,12 +1,12 @@
 
 import { Client } from "pg";
 import { loadEnvConfig } from "@next/env";
+import { faker } from "@faker-js/faker";
 
 const projectDir = process.cwd();
 loadEnvConfig(projectDir);
 
 async function loadFakeData(numUsers: number = 10) {
-    console.log(`executing load fake data. generating ${numUsers} users.`);
 
     const client = new Client({
         user: process.env.POSTGRES_USER,
@@ -17,8 +17,27 @@ async function loadFakeData(numUsers: number = 10) {
     });
     await client.connect();
 
-    const res = await client.query('select 1');
-    console.log('res: ', res);
+    try{
+        await client.query('begin');
+
+        for(let i = 0; i < numUsers; i ++){
+            console.log(`call index to ${i}`);
+            await client.query("insert into public.users (username, password, avatar) values ($1, $2, $3)",
+                [faker.internet.userName(), "password", faker.internet.avatar()]
+            );
+            console.log('pushed data');
+        }
+        
+        await client.query('commit');
+    }catch(error){
+        await client.query('rollback');
+        throw error
+    }finally{
+        console.log(`executing load fake data. generating ${numUsers} users.`);
+        await client.end();
+    }
 };
 
-loadFakeData();
+const numUsers = parseInt(process.argv[2]) || 10;
+console.log(`loading ${numUsers} fake users.`);
+loadFakeData(numUsers);
