50446d146a0d3974abe22096022b89c2a0b304fb
酒井専冴 <atsukisakai@sakaisensaenoMacBook-Pro.local>
2023-10-30 00:58:28 +0900

    load fake data to class method

--- script/load-fake-data.ts
+++ script/load-fake-data.ts
@@ -2,71 +2,144 @@
 import { Client } from "pg";
 import { loadEnvConfig } from "@next/env";
 import { faker } from "@faker-js/faker";
+import bcrypt from "bcrypt";
 
-const projectDir = process.cwd();
-loadEnvConfig(projectDir);
+class DataLoader {
 
-async function loadFakeData(numUsers: number = 10) {
+    // - Properties
+    private client: Client;
+    private generateCount: number;
 
-    const client = new Client({
+    // - Constructor
+    constructor(gennerateCount: number = 10) {
+        this.generateCount = genenrateCount;
+        const projectDir = process.cwd();
+        loadEnvConfig(projectDir);
+
+        this.client = new Client({
         user: process.env.POSTGRES_USER,
         host: process.env.POSTGRES_HOST,
         database: process.env.POSTGRES_NAME,
         password: process.env.POSTGRES_PASSWORD,
-        port: parseInt(process.env.POSTGRES_PORT!)
-    });
-    await client.connect();
+        port: parseInt(process.env.POSTGRES_PORT!),
+        });
+    }
 
-    try{
-        await client.query('begin');
-        // users data
-        for(let i = 0; i < numUsers; i ++){
-            await client.query("insert into public.users (username, password, avatar) values ($1, $2, $3)",
-                [faker.internet.userName(), "password", faker.internet.avatar()]
-            );
+    // - Functions
+    async dbConnect() {
+        await this.client.connect();
+    }
+
+    async disconnect() {
+        await this.client.end();
+    }
+
+    async beginTransaction() {
+        await this.client.query('begin');
+    }
+
+    async commitTransaction() {
+        await this.client.query('commit');
+    }
+
+    async rollbackTransaction() {
+        await this.client.query('rollback');
+    }
+
+    async insertUser() {
+        const username = faker.internet.userName();
+        const avatar = faker.internet.avatar();
+        const saltRounds  =10;
+        const hash = await bcrypt.hash("social_media_app", saltRounds);
+
+        await this.client.query("insert into public.users (username, password, avatar) values ($1, $2, $3)",
+        [username, hash, avatar]
+        );
+    }
+
+    async loadUserData() {
+        try {
+        await this.beginTransaction();
+
+        for (let i = 0; i < genenrateCount; i++) {
+            await this.insertUser();
         }
 
+        await this.commitTransaction();
+        } catch (error) {
+        await this.rollbackTransaction();
+        throw error;
+        }
+    }
 
-        // posts data
-        const res = await client.query(
+    async loadPostsData() {
+        try {
+        await this.beginTransaction();
+
+        const res = await this.client.query(
             "select id from public.users order by created_at desc limit $1",
-            [numUsers]
+            [genenrateCount]
         );
 
-        console.log('rows: ', res.rows);
-
-        for(const row of res.rows) {
-            for(let i = 0; i < Math.ceil(Math.random() * 50); i++) {
-                await client.query(
-                    "insert into public.posts (user_id, content) values ($1, $2)",
-                    [row.id, faker.lorem.sentence()]
-                )
+        for (const row of res.rows) {
+            for (let i = 0; i < genenrateCount; i++) {
+            await this.client.query(
+                "insert into public.posts (user_id, content) values ($1, $2)",
+                [row.id, faker.lorem.sentence()]
+            )
             }
-        };
+        }
+
+        await this.commitTransaction();
+        } catch (error) {
+        await this.rollbackTransaction();
+        throw error;
+        }
+    }
+
+    async loadFollowsData() {
+        try {
+        await this.beginTransaction();
+
+        const res = await this.client.query(
+            "select id from public.users"
+        );
 
         for (const row of res.rows) {
             for (const row2 of res.rows) {
-                if(row.idi != row2.id) {
-                    if(Math.random() > 0.5) {
-                        await client.query(
-                            "insert into follows (user_id, follower_id) values ($1, $2)",
-                            [row.id, row2.id]
-                        )
-                    }
+            if (row.id !== row2.id) {
+                if (Math.random() > 0.5) {
+                await this.client.query(
+                    "insert into follows (user_id, follower_id) values ($1, $2)",
+                    [row.id, row2.id]
+                )
                 }
             }
+            }
         }
-        
-        await client.query('commit');
-    }catch(error){
-        await client.query('rollback');
-        throw error
-    }finally{
-        console.log(`executing load fake data. generating ${numUsers} users.`);
-        await client.end();
-    }
-};
 
-const numUsers = parseInt(process.argv[2]) || 10;
-console.log(`loading ${numUsers} fake users.`);
-loadFakeData(numUsers);
+        await this.commitTransaction();
+        } catch (error) {
+        await this.rollbackTransaction();
+        throw error;
+        }
+    }
+}
+
+const genenrateCount = parseInt(process.argv[2]) || 10;
+const dataLoader = new DataLoader(genenrateCount);
+
+dataLoader.dbConnect().then(async () => {
+    console.log('db connected.');
+    await dataLoader.loadUserData();
+    console.log('created users');
+    await dataLoader.loadPostsData();
+    console.log('created posts');
+    await dataLoader.loadFollowsData();
+    console.log('created follows');
+}).catch((error) => {
+    console.error("Error: ", error);
+}).finally(() => {
+    dataLoader.disconnect();
+    console.log('db disconnected');
+})
