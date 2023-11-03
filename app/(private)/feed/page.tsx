2c523afd5205689cc8e6433682e1000e66c40958
酒井専冴 <atsukisakai@sakaisensaenoMacBook-Pro.local>
2023-11-04 07:08:05 +0900

    fix 500 error and api feed

--- app/(private)/feed/page.tsx
+++ app/(private)/feed/page.tsx
@@ -1,9 +1,18 @@
+import fetcher from "@/app/utils/fetcher";
 import React from "react";
+import useSWR from "swr";
 
 const Feed = () => {
+  // const { data: feeds, error, isLoading } = useSWR("/api/posts/feed", fetcher);
+
+  // if (error) return <div>{error}</div>;
+  // if (isLoading) return <div>loading now...</div>;
   return (
     <main>
       <h2>Feed</h2>
+      {/* {feeds.map((feed: any) => {
+        return <div key={feed.id}>{feed.username}</div>;
+      })} */}
     </main>
   );
 };
