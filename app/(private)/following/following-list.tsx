610ce427571b37782ca7a14ab3b13c5a65c979ec
酒井専冴 <atsukisakai@sakaisensaenoMacBook-Pro.local>
2023-12-15 23:07:32 +0900

    refacter

--- app/(private)/following/following-list.tsx
+++ app/(private)/following/following-list.tsx
@@ -3,6 +3,7 @@ import useSWR from "swr";
 import Image from "next/image";
 
 import type { UserInfo } from "@/app/types";
+import { UserIcon } from "@heroicons/react/20/solid";
 
 function FollowingList({ index }: { index: number }) {
   const { data: userData } = useSWR("/api/users/profile");
@@ -20,13 +21,20 @@ function FollowingList({ index }: { index: number }) {
       {followerData.data.map((follower: UserInfo) => {
         return (
           <li key={follower.id} className="flex items-center p-2">
-            <Image
-              className="w-[32px] h-[32px] rounded-full"
-              width={32}
-              height={32}
-              src={follower.avatar}
-              alt={follower.username}
-            />
+            {follower.avatar ? (
+              <Image
+                className="w-[32px] h-[32px] rounded-full"
+                width={32}
+                height={32}
+                src={follower.avatar ?? ""}
+                alt={follower.username}
+              />
+            ) : (
+              <div className="bg-gray-500 p-2 rounded-full w-[32px] h-[32px] flex justify-center items-center">
+                <UserIcon width={20} height={20} />
+              </div>
+            )}
+
             <p className=" text-white text-xs tracking-wide pl-3">
               {follower.username}
             </p>
