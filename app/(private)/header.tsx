80f5e76a0b9244e1e07d15c2caeec380d3a6005b
酒井専冴 <atsukisakai@sakaisensaenoMacBook-Pro.local>
2023-12-27 14:45:21 +0900

    refacter

--- app/(private)/header.tsx
+++ app/(private)/header.tsx
@@ -12,15 +12,18 @@ const Header = () => {
   } = useSWR("/api/users/profile");
 
   if (userError) return <div>failed to {userError}</div>;
+  if (!userRes || isLoading) return <div>loading</div>;
+
+  console.log("userRes: ", userRes);
 
   return (
     <header className="h-[60px] w-full p-5 bg-slate-900 fixed top-0 left-0 right-0">
       <div className="flex flex-row justify-between items-center max-w-md mx-auto">
         <h1 className=" blockfont-mono">Social Media App</h1>
-        {isLoading ? (
+        {isLoading || !userRes ? (
           <WaitingLoader />
         ) : (
-          userRes && <UserLabel user={userRes.data} href={"/account"} />
+          <UserLabel user={userRes.data} href={"/account"} />
         )}
       </div>
     </header>
